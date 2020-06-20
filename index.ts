ed8d3101d17620b0a01c98621fc4b8e71be2207a
Desmond Brand <dmnd@desmondbrand.com>
2020-06-20 16:16:19 -0700

    calling the twitter api works

--- index.ts
+++ index.ts
@@ -1,16 +1,28 @@
-import Twitter from "twitter-lite";
+require("dotenv").config();
+const Twitter = require("twitter-lite");
 
 const entryPoint = "1266401790230069248";
-console.log(entryPoint);
 
-const user = new Twitter({
-  consumer_key: "abc",
-  consumer_secret: "def",
+async function main() {
+  const user = new Twitter({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+  });
+
+  const response = await user.getBearerToken();
+
+  const app = new Twitter({
+    bearer_token: response.access_token,
+  });
+
+  const result = await app.get(`statuses/show/${entryPoint}`);
+  console.log(result);
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
 
-const response = await user.getBearerToken();
-console.log(response);
-
-// const app = new Twitter({
-//   bearer_token: response.access_token,
-// });
+// ???
+export {};
